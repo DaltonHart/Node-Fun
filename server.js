d1d77aa1c4cd9f5259d99a6589722445793bdd83
Dalton Hart <DaltonVaren@gmail.com>
2019-02-19 10:01:06 -0800

    init commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+let cities = []
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html' , { root : __dirname});
+})
+
+app.post('/api/cities', (request, response) => {
+  console.log(request.body)
+  let newCity = { name: request.body.name, description: request.body.description };
+  cities.push(newCity);
+  response.json(cities);
+});
+
+const server = app.listen(3000);
