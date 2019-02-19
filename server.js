a6b79d13d45f6e8b64d4c5f32b1e10178e0cb1e9
Dalton Hart <31603649+DaltonHart@users.noreply.github.com>
2019-02-19 10:35:16 -0800

    changed cities to places
    I know I'm being crazy changing this again

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.sendFile('views/index.html' , { root : __dirname});
 })
 
-app.post('/api/towns', (req, res) => {
+app.post('/api/places', (req, res) => {
   console.log(req.body)
   let newCity = { name: req.body.name, description: req.body.description };
   cities.push(newCity);
